7981f671179c2066ec812a0fd472e4405f1f97ba
Steve Farrell <sparticlesteve@gmail.com>
2019-02-13 13:41:38 -0800

    using user and job specific sync file

--- train.py
+++ train.py
@@ -53,8 +53,10 @@ def init_workers(distributed=False):
     rank = int(os.environ['SLURM_PROCID'])
     n_ranks = int(os.environ['SLURM_NTASKS'])
     if distributed:
+        sync_file = 'file:///tmp/%s_%s_pytorch_sync' % (
+            os.environ['USER'], os.environ['SLURM_JOB_ID'])
         dist.init_process_group(backend='gloo', world_size=n_ranks, rank=rank,
-                                init_method='file:///tmp/sfarrell/pytorch_sync_file')
+                                init_method=sync_file)
     return rank, n_ranks
 
 def load_config(config_file):
