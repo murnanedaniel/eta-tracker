82f76a718f13870351ccbd8db1ac078576f9ae0c
Steve Farrell <sparticlesteve@gmail.com>
2019-02-27 13:29:57 -0800

    single-gpu med007 training

--- scripts/train_med_cgpu.sh
+++ scripts/train_med_cgpu.sh
@@ -3,15 +3,31 @@
 #SBATCH -N 1
 #SBATCH -C gpu
 #SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:8
-#SBATCH --exclusive
+#SBATCH --gres=gpu:1
 #SBATCH -t 2:00:00
 #SBATCH -o logs/%x-%j.out
 
+# Disabled slurm config
+###SBATCH --exclusive
+###SBATCH --gres=gpu:8
+
+config=configs/segclf_med.yaml
+
 mkdir -p logs
+
+# Temporarily use project instead of scratch
 export SCRATCH=/project/projectdirs/nstaff/sfarrell
-conda activate /global/common/cori/software/pytorch/v1.0.0-gpu
+
+# Setup software
+#conda activate /global/common/cori/software/pytorch/v1.0.0-gpu
+module load pytorch/v1.0.0-gpu
+
+# Setup MPI enabled software
 #conda activate /project/projectdirs/nstaff/sfarrell/conda/pytorch/v1.0.1-gpu
 #export MV2_ENABLE_AFFINITY=0
 
-srun -n 8 -l -u python train.py configs/segclf_med.yaml -d --rank-gpu
+# 1 GPU
+srun -u python train.py $config --gpu 0 $@
+
+# 8 GPUs
+#srun -n 8 -l -u python train.py $config -d --rank-gpu $@
