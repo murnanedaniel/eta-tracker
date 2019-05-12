01cf2e4ba8e01b39f0b4c05b1b00c2e6cdc30d9a
Steve Farrell <sparticlesteve@gmail.com>
2019-05-12 16:49:02 -0700

    renaming module files for coherency

--- models/__init__.py
+++ models/__init__.py
@@ -10,10 +10,10 @@ def get_model(name, **model_args):
         from .cnn_classifier import CNNClassifier
         return CNNClassifier(**model_args)
     elif name == 'gnn_segment_classifier':
-        from .gnn import GNNSegmentClassifier
+        from .agnn_original import GNNSegmentClassifier
         return GNNSegmentClassifier(**model_args)
     elif name == 'gnn_sparse':
-        from .gnn_sparse import GNNSegmentClassifier
+        from .agnn import GNNSegmentClassifier
         return GNNSegmentClassifier(**model_args)
     elif name == 'mpnn':
         from .mpnn import GNN
