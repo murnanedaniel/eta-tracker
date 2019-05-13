37b342bb607dea9fb208443279e0edf9194d3ca2
Steve Farrell <sparticlesteve@gmail.com>
2019-05-12 17:05:02 -0700

    removed unused cnn classifier from models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -6,10 +6,7 @@ def get_model(name, **model_args):
     """
     Top-level factory function for getting your models.
     """
-    if name == 'cnn_classifier':
-        from .cnn_classifier import CNNClassifier
-        return CNNClassifier(**model_args)
-    elif name == 'gnn_segment_classifier':
+    if name == 'gnn_segment_classifier':
         from .agnn_original import GNNSegmentClassifier
         return GNNSegmentClassifier(**model_args)
     elif name == 'gnn_sparse':
