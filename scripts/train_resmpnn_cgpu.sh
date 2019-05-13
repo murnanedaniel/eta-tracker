c2f7b2118fbe50a6af1e35b75b98db18e775252f
Steve Farrell <sparticlesteve@gmail.com>
2019-05-12 20:24:11 -0700

    added res mpnn submit script for cori-gpu

---
+++ scripts/train_resmpnn_cgpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J resmpnn-big-cgpu
+#SBATCH -N 1
+#SBATCH -C gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=30GB
+#SBATCH -t 4:00:00
+#SBATCH -o logs/%x-%j.out
+
+# Setup
+config=configs/resmpnn_big.yaml
+. scripts/setup_cgpu.sh
+mkdir -p logs
+
+# Single GPU training
+srun -u python train.py $config --gpu 0 $@
