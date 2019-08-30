578b8900d68b81ebf0f04c3f228fbd2e6b9f67da
Steve Farrell <sparticlesteve@gmail.com>
2019-08-30 10:44:22 -0700

    update setup files for pytorch v1.2.0

--- scripts/setup_cgpu.sh
+++ scripts/setup_cgpu.sh
@@ -1,5 +1,8 @@
 # Software environment setup for Cori GPU
-# Currently using my conda installation with pytorch-geometric
-INSTALL_DIR=/global/cscratch1/sd/sfarrell/conda/pytorch-geometric-gpu
-conda activate $INSTALL_DIR
-export LD_LIBRARY_PATH=$INSTALL_DIR/lib:$LD_LIBRARY_PATH
+module load gcc/7.3.0
+module load cuda/10.1.168
+module load openmpi/4.0.1-ucx-1.6
+module load pytorch/v1.2.0-gpu
+
+# Libary path fix
+export LD_LIBRARY_PATH=$(dirname $(which python))/../lib/python3.6/site-packages/torch/lib:$LD_LIBRARY_PATH
