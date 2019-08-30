61d26bf72b12a034bd495fb2db83f299faaa0f88
Steve Farrell <sparticlesteve@gmail.com>
2019-08-30 11:17:15 -0700

    single gpu training for agnn

--- scripts/train_agnn_cgpu.sh
+++ scripts/train_agnn_cgpu.sh
@@ -3,8 +3,9 @@
 #SBATCH -N 1
 #SBATCH -C gpu
 #SBATCH -t 8:00:00
-#SBATCH --gres=gpu:8
-#SBATCH --exclusive
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=30GB
 #SBATCH -d singleton
 #SBATCH -o logs/%x-%j.out
 
@@ -14,8 +15,8 @@ mkdir -p logs
 . scripts/setup_cgpu.sh
 
 # Single GPU training
-#srun -u python train.py $config --gpu 0 $@
+srun -u python train.py $config --gpu 0 $@
 
 # Multi-GPU training with MPI
-srun -u -l --ntasks-per-node 8 \
-    python train.py $config --rank-gpu -d ddp-mpi $@
+#srun -u -l --ntasks-per-node 8 \
+#    python train.py $config --rank-gpu -d ddp-mpi $@
